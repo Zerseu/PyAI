7276dc4bcab2e76d0cbd377f19f47937a44b57bb
Alexandru-Ion Marinescu <cc_plusplus@yahoo.com>
2022-05-04 17:31:56 +0300

    Started working on JARVIS...

--- jarvis/recognition.py
+++ jarvis/recognition.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 
-with sr.AudioFile('The phone is ringing.wav') as source:
+with sr.Microphone() as source:
     r = sr.Recognizer()
     r.energy_threshold = 1200
-    print(r.recognize_google(r.record(source)))
+    print(r.recognize_google(r.record(source, duration=10)))
